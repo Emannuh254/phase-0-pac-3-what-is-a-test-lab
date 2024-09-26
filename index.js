19322fb0d2dd4b2119be984a29df014afaa543ea
Emmanuel Mutugi <smontana025@gmail.com>
2024-09-26 11:38:47 +0300

    mannuh

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 30;
 const message = `${name} is ${height} inches tall`;
+
+describe("Height", () => {
+    it("is less than 40 and greater than 0", () => {
+      expect(height).toBeGreaterThan(0);
+      expect(height).toBeLessThan(40);
+    });
+});
 
 module.exports = {name, height, message};
